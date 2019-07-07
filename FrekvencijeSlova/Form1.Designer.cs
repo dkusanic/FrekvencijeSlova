a9af112375ac2acd5c86de2ec782bc333df2aab8
Domagoj Kusanic <dkusanic@foi.hr>
2019-07-07 10:55:58 +0200

    Napravljen GUI
    napravljen izgled forme prema uputama

--- FrekvencijeSlova/Form1.Designer.cs
+++ FrekvencijeSlova/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FrekvencijeSlova
 {
-    partial class Form1
+    partial class FrmFrekvencije
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtBoxUnos = new System.Windows.Forms.TextBox();
+            this.txtBoxIspis = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBoxUnos
+            // 
+            this.txtBoxUnos.Location = new System.Drawing.Point(12, 12);
+            this.txtBoxUnos.Multiline = true;
+            this.txtBoxUnos.Name = "txtBoxUnos";
+            this.txtBoxUnos.Size = new System.Drawing.Size(647, 426);
+            this.txtBoxUnos.TabIndex = 0;
+            // 
+            // txtBoxIspis
+            // 
+            this.txtBoxIspis.Location = new System.Drawing.Point(665, 12);
+            this.txtBoxIspis.Multiline = true;
+            this.txtBoxIspis.Name = "txtBoxIspis";
+            this.txtBoxIspis.Size = new System.Drawing.Size(123, 426);
+            this.txtBoxIspis.TabIndex = 1;
+            // 
+            // FrmFrekvencije
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.txtBoxIspis);
+            this.Controls.Add(this.txtBoxUnos);
+            this.Name = "FrmFrekvencije";
+            this.Text = "Frekvencije slova";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtBoxUnos;
+        private System.Windows.Forms.TextBox txtBoxIspis;
     }
 }
 
--- FrekvencijeSlova/Form1.cs
+++ FrekvencijeSlova/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FrekvencijeSlova
 {
-    public partial class Form1 : Form
+    public partial class FrmFrekvencije : Form
     {
-        public Form1()
+        public FrmFrekvencije()
         {
             InitializeComponent();
         }
